e1e1f5b2c6367d5a82fa00611a61c4e0a54c69e5
edRRR <r03r000@gmail.com>
2023-08-19 19:37:50 +0200

    sigo practicanto fetch

--- app/rickandmortyapi/page.jsx
+++ app/rickandmortyapi/page.jsx
@@ -1,9 +1,18 @@
-fetch('rickandmortyapi')
-export default function Caracters() {
-  return (
-  <div>
-    rickandmortyapi</div>
-  
-  )
-  
+import { Suspense } from "react";
+
+async function getCharacters() {
+  try {
+    const res = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return { results: [] }; // retornar datos vacíos
+  }
+}
+
+export default function Characters() {
+  const data = getCharacters();
+
+  return;
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,55 @@
 import Link from "next/link";
-import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="flex rounded-sm ">
-      <h1>Navbar</h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/tienda">Tienda</Link>
-        </li>
-        <li>
-          <Link href="/post">Post</Link>
-        </li>
-        <li>
-          <Link href="/rickandmortyapi">Caracters </Link>
-        </li>
+    <nav className="rounded border-solid hover:border-dotted">
+      <h1>NavBar</h1>
+      <ul className="  space-x-2   flex-row    ">
+        <Link
+          className="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 
+        hover:bg-blue-700 text-white
+"
+          href="/"
+        >
+          Home
+        </Link>
+
+        <Link
+          className="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 
+        hover:bg-blue-700 text-white
+"
+          href="/about"
+        >
+          About
+        </Link>
+
+        <Link
+          className="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 
+        hover:bg-blue-700 text-white
+"
+          href="/tienda"
+        >
+          Tienda
+        </Link>
+
+        <Link
+          className="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 
+        hover:bg-blue-700 text-white
+"
+          href="/post"
+        >
+          Post
+        </Link>
+
+        <Link
+          className="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 
+        hover:bg-blue-700 text-white
+"
+          href="/rickandmortyapi"
+        >
+          Rickandmorty
+        </Link>
       </ul>
-    </div>
+    </nav>
   );
 }
